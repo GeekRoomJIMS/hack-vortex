4ba78422e882b1909b31468ed1def89ff542f977
sanchxt <tpbeast007@gmail.com>
2024-08-26 23:56:36 +0530

    fix incorrect id issue

--- components/main/Timeline.tsx
+++ components/main/Timeline.tsx
@@ -52,7 +52,7 @@ const Timeline = () => {
   return (
     <section
       className="flex flex-col items-center justify-center py-20"
-      id="sponsors"
+      id="timeline"
     >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         Hackathon Timeline
